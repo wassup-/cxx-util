188abbfb0c1406428cfda737e7b02da805d5f447
Tom Knapen <tom_knapen@student.uhasselt.be>
2015-12-16 22:29:55 +0100

    filesystem access-related changes

--- filesystem/access.hpp
+++ filesystem/access.hpp
@@ -9,33 +9,44 @@ namespace filesystem
 namespace detail
 {
 
-enum class filesystem_user_e {
+template<typename Enum, typename = std::enable_if_t<std::is_enum<Enum>::value>>
+typename std::underlying_type<Enum>::type underlying_value(Enum e)
+{ return static_cast<typename std::underlying_type<Enum>::type>(e); }
+
+enum class filesystem_user_e : int {
   all   = 0,
   user  = 1,
   group = 2,
   other = 4,
 };
 
-enum class filesystem_mode_e {
+enum class filesystem_mode_e : int {
   none    = 0,
   read    = 1,
   write   = 2,
   execute = 4,
 };
 
+filesystem_mode_e operator|(filesystem_mode_e left, filesystem_mode_e right)
+{ return static_cast<filesystem_mode_e>(underlying_value(left) | underlying_value(right)); }
+
 template<filesystem_user_e user>
 struct filesystem_user : std::integral_constant<filesystem_user_e, user> { };
 
 template<filesystem_mode_e mode>
-struct filesystem_mode : std::integral_constant<filesystem_mode_e, mode> { };
+struct filesystem_mode : std::integral_constant<filesystem_mode_e, mode>
+{
+  template<filesystem_mode_e other_mode>
+  filesystem_mode<(mode | other_mode)> operator|(filesystem_mode<other_mode>) { return { }; }
+};
 
 } // namespace detail
 
 namespace users
 {
 
-constexpr static detail::filesystem_user<detail::filesystem_user_e::all>   all { };
-constexpr static detail::filesystem_user<detail::filesystem_user_e::user>  user { };
+constexpr static detail::filesystem_user<detail::filesystem_user_e::all>   all   { };
+constexpr static detail::filesystem_user<detail::filesystem_user_e::user>  user  { };
 constexpr static detail::filesystem_user<detail::filesystem_user_e::group> group { };
 constexpr static detail::filesystem_user<detail::filesystem_user_e::other> other { };
 
@@ -44,10 +55,10 @@ constexpr static detail::filesystem_user<detail::filesystem_user_e::other> other
 namespace modes
 {
 
-constexpr static detail::filesystem_mode<detail::filesystem_mode_e::none>    none = { };
-constexpr static detail::filesystem_mode<detail::filesystem_mode_e::read>    read = { };
-constexpr static detail::filesystem_mode<detail::filesystem_mode_e::write>   write = { };
-constexpr static detail::filesystem_mode<detail::filesystem_mode_e::execute> execute = { };
+constexpr static detail::filesystem_mode<detail::filesystem_mode_e::none>    none    { };
+constexpr static detail::filesystem_mode<detail::filesystem_mode_e::read>    read    { };
+constexpr static detail::filesystem_mode<detail::filesystem_mode_e::write>   write   { };
+constexpr static detail::filesystem_mode<detail::filesystem_mode_e::execute> execute { };
 
 } // namespace modes
 
--- filesystem/exceptions.hpp
+++ filesystem/exceptions.hpp
@@ -12,10 +12,22 @@ struct filesystem_exception : std::runtime_error
   using std::runtime_error::runtime_error;
 };
 
+struct filesystem_access_exception : filesystem_exception
+{
+  using filesystem_exception::filesystem_exception;
+};
+
 struct file_not_found_exception : filesystem_exception
 {
-  file_not_found_exception(const std::string& filename)
-  : filesystem_exception("File " + filename + " not found")
+  file_not_found_exception(const std::string &path)
+  : filesystem_exception("File " + path + " not found")
+  { }
+};
+
+struct directory_not_found_exception : filesystem_exception
+{
+  directory_not_found_exception(const std::string &path)
+  : filesystem_exception("Directory " + path + " not found")
   { }
 };
 
